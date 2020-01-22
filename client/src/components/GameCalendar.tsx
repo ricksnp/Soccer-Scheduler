3e2f96f012e34002c0bf2056a485b903c968ab56
lilaht <lilahthibodeaux@gmail.com>
2020-01-22 17:15:41 -0600

    change calendar

--- client/src/components/GameCalendar.tsx
+++ client/src/components/GameCalendar.tsx
@@ -1,27 +1,38 @@
 import React, { useState } from 'react';
-import Calendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import { EventInput } from '@fullcalendar/core';
-import interactionPlugin from '@fullcalendar/interaction';
-import '../style/gameCalendar.scss';
+import  {Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../style/gameCalendar.css'
 
-
+const localizer = momentLocalizer(moment);
 
 const GameCalendar = () => {
 
-  const [events, setEvents] = useState();
+  const [events, setEvents] = useState([
+    {
+      'title': 'All Day Event very long title',
+      'start': new Date(2020, 3, 0),
+      'end': new Date(2020, 3, 1),
+      'allDay': true,
+    },
+    {
+      'title': 'Long Event',
+      'start': new Date(2020, 3, 7),
+      'end': new Date(2020, 3, 10)
+    },
+  
+    {
+      'title': 'DTS STARTS',
+      'start': new Date(2020, 2, 13, 0, 0, 0),
+      'end': new Date(2020, 2, 20, 0, 0, 0)
+    }
+  ]);
 
   return(
       <div className="game-cal">
           <Calendar 
-            defaultView="dayGridMonth"  
-            header={{
-              left: 'prev, next, today',
-              center: 'title',
-              right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek'
-            }}
-            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+            localizer={localizer}
+            events={events}
           />
       </div>
   );
