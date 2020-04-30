aac4b78df48d72f664d4b470e7ba530d78f4ed93
Nathan Ricks <rickspnathan@gmail.com>
2020-04-30 02:43:14 -0500

    enhanced email stuff

--- client/src/pages/home/AssignorExport.tsx
+++ client/src/pages/home/AssignorExport.tsx
@@ -1,23 +1,23 @@
-import React, {useState} from 'react'
-import {Button} from 'antd';
+import React, { useState } from 'react'
+import { Button } from 'antd';
 import { CSVLink } from 'react-csv';
-import {apiGetGames} from '../../utility/APIGameControl';
+import { apiGetGames } from '../../utility/APIGameControl';
 import arbiter from '../../components/Arbiter.json';
 import { setFlagsFromString } from 'v8';
-import {getOnlyScheduledGames} from '../../components/Games';
+import { getOnlyScheduledGames } from '../../components/Games';
 import { schoolNames } from '../coach/components/autofill';
 
 const header = [
-    {label: "Date", key: "date"},
-    {label: "Time", key: "time"},
-    {label: "Level", key: "level"},
-    {label: "Home-Team", key: "home"},
-    {label: "Home-Level", key: "hLevel"},
-    {label: "Away-Team", key: "away"},
-    {label: "Away-Level", key: "aLevel"}
+    { label: "Date", key: "date" },
+    { label: "Time", key: "time" },
+    { label: "Level", key: "level" },
+    { label: "Home-Team", key: "home" },
+    { label: "Home-Level", key: "hLevel" },
+    { label: "Away-Team", key: "away" },
+    { label: "Away-Level", key: "aLevel" }
 ]
 
- const initialResponse: any = [{
+const initialResponse: any = [{
     status: "null",
     homeTeamName: "null",
     awayTeamName: "null",
@@ -30,7 +30,7 @@ const header = [
 
 
 //FOR WHOEVER IS ABOUT TO EDIT THIS COMPONENT IM SO SORRY AND GOOD LUCK
-const AssignorExport = (newData:any) => {
+const AssignorExport = (newData: any) => {
 
     const [csvData, setCSVData] = useState("");
     const [counter, setCounter] = useState(0);
@@ -38,50 +38,42 @@ const AssignorExport = (newData:any) => {
     const [scheduled, setScheduled] = useState(newData.newData);
     const [flag, setFlag] = useState(false)
 
-    if(counter == 0)
-    {
-    
+    if (counter == 0) {
+
         setCounter(counter + 1)
 
-        let arbiterInfo:any = [];
+        let arbiterInfo: any = [];
 
-        if(scheduled[0] != undefined && scheduled[0].status != 'null')
-        {
+        if (scheduled[0] != undefined && scheduled[0].status != 'null') {
             //arbiter map key = school name not for arbiter
             //value = level, gender, arbiter name
             const arbiterMap = new Map();
-            
-            for(let i = 0; i< arbiter.school.length; i++)
-            {
+
+            for (let i = 0; i < arbiter.school.length; i++) {
                 var newGen;
                 var newL;
 
-                if(arbiter.school[i].level == "varsity")
-                {
+                if (arbiter.school[i].level == "varsity") {
                     newL = "v"
                 }
-                else if(arbiter.school[i].level == "any")
-                {
+                else if (arbiter.school[i].level == "any") {
                     newL = "a"
                 }
-                else{
+                else {
                     newL = "jv"
                 }
 
-                if(arbiter.school[i].gender == "boys")
-                {
+                if (arbiter.school[i].gender == "boys") {
                     newGen = "b"
                 }
-                else if(arbiter.school[i].gender == "any")
-                {
+                else if (arbiter.school[i].gender == "any") {
                     newGen = "a"
                 }
-                else
-                {
+                else {
                     newGen = "g"
                 }
                 var key = arbiter.school[i].name + " " + newL + " " + newGen;
-                var mapData = {level: arbiter.school[i].level, gender: arbiter.school[i].gender, arbiter: arbiter.school[i].arbiter }
+                var mapData = { level: arbiter.school[i].level, gender: arbiter.school[i].gender, arbiter: arbiter.school[i].arbiter }
 
                 arbiterMap.set(key, mapData)
             }
@@ -95,71 +87,59 @@ const AssignorExport = (newData:any) => {
 
             //Reading all keys in arbiterMap, comparing it to the scheduled game hometeam or awayTeam,
             // if the scheduled hometeam/awayteam matches 
-            for(let i = 0; i < arbiterMap.size; i++)
-            {
+            for (let i = 0; i < arbiterMap.size; i++) {
                 var nextName = iterator.next().value;
 
-                for(let j =0;j < scheduled.length; j++)
-                {
+                for (let j = 0; j < scheduled.length; j++) {
                     //basic name in arbiter (not arbiter name)
 
 
-                    if(scheduled[j] != undefined && nextName != undefined && (nextName.includes(scheduled[j].home)))
-                    {
+                    if (scheduled[j] != undefined && nextName != undefined && (nextName.includes(scheduled[j].home))) {
                         var newLevel = "jv";
                         var newgender = "boys"
                         var newObj = arbiterMap.get(nextName);
 
-        
 
-                        if(scheduled[j].teamLevel == "v")
-                        {
+
+                        if (scheduled[j].teamLevel == "v") {
                             newLevel = "varsity"
                         }
-                        if(scheduled[j].gender == "b")
-                        {
+                        if (scheduled[j].gender == "b") {
                             newgender = "boys"
                         }
-                        else if(scheduled[j].gender == "g")
-                        {
+                        else if (scheduled[j].gender == "g") {
                             newgender = "girls"
                         }
 
 
-                        if(newObj.level == newLevel && newObj.gender == newgender)
-                        {
+                        if (newObj.level == newLevel && newObj.gender == newgender) {
                             let ourKey = scheduled[j].home + "" + scheduled[j].gender + "" + scheduled[j].teamLevel
-                            newMap.set(ourKey , newObj.arbiter)
+                            newMap.set(ourKey, newObj.arbiter)
                         }
                     }
-                    if (scheduled[j] != undefined && nextName != undefined && nextName.includes(scheduled[j].away))
-                    {
+                    if (scheduled[j] != undefined && nextName != undefined && nextName.includes(scheduled[j].away)) {
 
                         var newLevel = "jv";
                         var newgender = "boys"
                         var newObj = arbiterMap.get(nextName);
 
 
-                        if(scheduled[j].teamLevel == "v")
-                        {
+                        if (scheduled[j].teamLevel == "v") {
                             newLevel = "varsity"
                         }
-                        if(scheduled[j].gender == "b")
-                        {
+                        if (scheduled[j].gender == "b") {
                             newgender = "boys"
                         }
-                        else if(scheduled[j].gender == "g")
-                        {
+                        else if (scheduled[j].gender == "g") {
                             newgender = "girls"
                         }
 
 
-                        if(newObj.level == newLevel && newObj.gender == newgender)
-                        {
+                        if (newObj.level == newLevel && newObj.gender == newgender) {
 
-                           
+
                             let ourKey = scheduled[j].away + "" + scheduled[j].gender + "" + scheduled[j].teamLevel
-                            newMap.set(ourKey , newObj.arbiter)
+                            newMap.set(ourKey, newObj.arbiter)
                         }
 
                     }
@@ -167,27 +147,23 @@ const AssignorExport = (newData:any) => {
             }
 
 
-            for(let i =0;i < scheduled.length; i++)
-            {
+            for (let i = 0; i < scheduled.length; i++) {
                 var temp = scheduled[i].start
 
                 var [date, time] = temp.split('T');
-                var [year,month,day] = date.split("-");
+                var [year, month, day] = date.split("-");
 
-                var [h,m,s] = time.split(":");
+                var [h, m, s] = time.split(":");
                 var modifier = "AM";
 
-                if(h == "00")
-                {
+                if (h == "00") {
                     h = "12";
                 }
-                else if(h == "12")
-                {
+                else if (h == "12") {
                     h = "12"
                     modifier = "PM"
                 }
-                else if(h > 12)
-                {
+                else if (h > 12) {
                     h = h % 12;
                     modifier = "PM"
                 }
@@ -195,16 +171,13 @@ const AssignorExport = (newData:any) => {
                 var newLevel = "JV";
                 var newgender = "boys"
 
-                if(scheduled[i].teamLevel === "v")
-                {
+                if (scheduled[i].teamLevel === "v") {
                     newLevel = "Varsity"
                 }
-                if(scheduled[i].gender === "b")
-                {
+                if (scheduled[i].gender === "b") {
                     newgender = "boys"
                 }
-                else if(scheduled[i].gender === "g")
-                {
+                else if (scheduled[i].gender === "g") {
                     newgender = "girls"
                 }
 
@@ -213,7 +186,7 @@ const AssignorExport = (newData:any) => {
 
 
                 arbiterInfo.push({
-                    date: month +"/"+day+"/"+year.slice(-2),
+                    date: month + "/" + day + "/" + year.slice(-2),
                     time: h + ":" + m + " " + modifier,
                     level: newLevel,
                     home: newMap.get(homeKey),
@@ -229,13 +202,13 @@ const AssignorExport = (newData:any) => {
 
     }
 
-    return(
+    return (
         <>
-        {csvData == "" ? 
-            <>New Test</>
-        :
-            <Button><CSVLink data={csvData} headers={header}>Arbiter Export</CSVLink></Button>
-        }
+            {csvData == "" ?
+                <>New Test</>
+                :
+                <Button><CSVLink data={csvData} headers={header} className="hiddenOnPhone">Arbiter Export</CSVLink></Button>
+            }
         </>
     )
 }
